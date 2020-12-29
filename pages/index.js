d61d71370044838e3743c15d93f3a83b816f796b
sandrinejoy <sandrinejoy007@gmail.com>
2020-12-29 15:19:59 +0530

    hello, world!

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,12 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Fast Feedback
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+           By Sandrin Joy
+                </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
